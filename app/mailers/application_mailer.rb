b61460ba11442cf3141e10b1ed063f9016f2ddd1
Mason Roberts <mrobe18@gmail.com>
2024-08-27 09:54:34 -0400

    switched domain for mailersend

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "truckerjobs4u.admin@trial-neqvygmxmpjl0p7w.mlsender.net"
+  default from: "truckerjobs4u.admin@trial-3yxj6ljj767ldo2r.mlsender.net"
   layout "mailer"
 end
